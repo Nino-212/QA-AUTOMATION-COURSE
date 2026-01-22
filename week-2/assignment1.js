722188e318ee398a1da3f66cadcab723c18b17e1
Nino <tsikli.nino@gmail.com>
2026-01-21 23:13:28 -0500

    Add week-2 assignment1.js

---
+++ week-2/assignment1.js
@@ -0,0 +1,35 @@
+//Part 1
+
+const a = 10;
+const b = 5;
+
+const addResult = a + b;
+console.log(addResult);
+
+const substractResult = a - b;
+console.log(substractResult);
+
+const multiplyResult = a * b;
+console.log(multiplyResult);
+
+const divideResult = a / b;
+console.log(divideResult);
+
+//2 Increase
+let c = 5;
+c++;
+console.log(c);
+
+//Decrise
+let d = 6;
+d--;
+console.log(d);
+
+//Remainder
+let e = 10;
+let f = 5;
+console.log(e % f);
+
+//Raise it to the power
+const number = 15;
+console.log(number ** 2);
