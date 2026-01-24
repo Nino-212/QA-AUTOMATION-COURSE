eb8ab8a3e79e11546fde83a5ca4650f0831b3b6f
Nino <tsikli.nino@gmail.com>
2026-01-23 21:51:41 -0500

    week-2 assignment2.js

--- week-2/assignment2.js
+++ week-2/assignment2.js
@@ -8,3 +8,29 @@ console.log(march);
 
 let august = `summer`;
 console.log(august);
+
+//String Length
+
+let ice = "go home";
+console.log(ice.length);
+
+let winter = "I am tired of it";
+console.log(winter.length);
+
+const qa = `I'have had enough of bugs`;
+console.log(qa.length);
+
+let dog = "bark";
+console.log(dog.length);
+
+let cow = "moo";
+console.log(cow.length);
+
+let france = "Paris";
+console.log(france.length);
+
+let usa = "Washington";
+console.log(usa.length);
+
+let italy = "Rome";
+console.log(italy.length);
