f14a6075e5a7e0017c692199b8bb8d28f1a08ac3
Nino <tsikli.nino@gmail.com>
2026-01-24 15:53:10 -0500

    week-2?assignment2.js

--- week-2/assignment2.js
+++ week-2/assignment2.js
@@ -283,3 +283,18 @@ let colors = "red; blue; green";
 console.log(colors.split(""));
 console.log(colors.split(";"));
 console.log(colors.split(" "));
+//Arrays
+//Accessing array elements;
+
+let priorities = ["low", "medium", "high"];
+console.log(priorities[0]);
+console.log(priorities[1]);
+console.log(priorities[2]);
+
+let paymentStatment = ["pending", "paid", "overdue"];
+console.log(paymentStatment[1]);
+console.log(paymentStatment[0]);
+console.log(paymentStatment[2]);
+
+let jobLevels = ["junior", "middle", "senior"];
+console.log(jobLevels[2]);
