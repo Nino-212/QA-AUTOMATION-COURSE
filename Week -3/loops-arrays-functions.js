e494a323ed38cd072fef0919d612ff6e2f4b92b4
Nino <tsikli.nino@gmail.com>
2026-01-31 14:04:43 -0500

    loops-array-fuction.js

--- Week -3/loops-arrays-functions.js
+++ Week -3/loops-arrays-functions.js
@@ -1,3 +1,6 @@
+//Part 1
+// 🔁 Task 1 : Print All Elements
+
 let carBrands = ["BMW", "Tesla", "Audi"];
 
 function printCarBrands(arr) {
@@ -7,9 +10,18 @@ function printCarBrands(arr) {
 }
 printCarBrands(carBrands);
 
+// 🔁 Task 2: Index + Value
 function printCarBrandsAndIndex(arr) {
   for (let i = 0; i <= 2; i++) {
     console.log(i, arr[i]);
   }
 }
 printCarBrandsAndIndex(carBrands);
+
+// Task 3: First and Last
+
+function firstAndLast(arr) {
+  console.log(arr[0]);
+  console.log(arr[arr.length - 1]);
+}
+firstAndLast(carBrands);
