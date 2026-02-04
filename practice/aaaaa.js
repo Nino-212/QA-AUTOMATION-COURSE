63317678123598c6d710be7575bd8bf7153f0d7a
Nino <tsikli.nino@gmail.com>
2026-02-04 05:00:40 -0500

    aaaaa.js

---
+++ practice/aaaaa.js
@@ -0,0 +1,21 @@
+let num = 25;
+let string = "BMWX3";
+console.log(num === string);
+
+//2
+let age = 42;
+let goodAge = 45;
+console.log(age > goodAge);
+console.log(age < goodAge);
+
+//3
+let number = 100;
+let numberOne = 190;
+console.log(number !== numberOne);
+
+//4
+let numOne = 7;
+console.log(numOne >= 10);
+console.log(numOne < 5);
+
+console.log("" || "QA");
---
+++ week-4/ternary-operator.js
@@ -0,0 +1,16 @@
+//ternary-operator
+let age = 18;
+let message = 18 age >=18 ? "You are an adult." : "You are a minor";
+
+console.log (message);
+
+let ageTwo = 15;
+if (ageTwo >=18) {
+    console.log("You are an adult.");
+} else {
+    console.log ("You are a minor");
+}
+
+//example with ternary?if else 
+    let ageTwo= 26;
+if (ageTwo >=18) 
